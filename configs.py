1b95bf9502c797106553ea7ac78a9336cf221de2
luckykumarkansykar <106895560+luckykumarkansykar@users.noreply.github.com>
2024-01-19 17:05:09 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ class Config(object):
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002122577537"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6653167305"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001616985227")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001995794554"))
